a50a1f67f5c25f39985fe18a912dfa6429611e8d
ijgnd <fooijgnd@barijgnd>
2020-03-14 22:46:03 +0100

    next version all

--- src/__init__.py
+++ src/__init__.py
@@ -1,120 +1,78 @@
-# Copyright: 2020- ijgnd
-#            Ankitects Pty Ltd and contributors
+# Copyright: ijgnd
+#            AnKingMed
 # License: GNU AGPL, version 3 or later; http://www.gnu.org/licenses/agpl.html 
-#
-# This add-on contains a file named web/wikigears.svg from
-# https://commons.wikimedia.org/wiki/File:Gear-kegelzahnrad.svg
-# (backup: https://web.archive.org/web/20161207153533/https://commons.wikimedia.org/wiki/File:Gear-kegelzahnrad.svg)
-# that was originally uploaded according to that page by the wikimedia user "MyriamThyes"
-# It contains the following Licensing Information:
-#     I , the copyright holder of this work, release this work into the public domain. 
-#     This applies worldwide.
-#     In some countries this may not be legally possible; if so:
-#     I grant anyone the right to use this work for any purpose, 
-#     without any conditions, unless such conditions 
-#     are required by law.
-
-"""
-classic approach with wrap doesn't work: in main.py deckbrowser is instantiated 
-before add-ons are loaded. Arthur's "Deck name in title" used a custom wrapping
-function for bound methods to work around this. He plans to abandon this approach
-by adding a new style hook "deck_browser_did_render" to Anki, see below.
-
-
-new hooks don't help in 2020-03-01:
-- gui_hooks.deck_browser_did_render is only called if the containing method is called
-  with a non-default argument reuse=True which rarely happens
-  this hooks was added by Arthur for the add-on "Deck name in title"
-- gui_hooks.deck_browser_will_render_content(self, content): content is useless for me
-  since it is:
-          content = DeckBrowserContent(
-            tree=self._renderDeckTree(self._dueTree),
-            stats=self._renderStats(),
-            countwarn=self._countWarn(),
-        )
-- gui_hooks.deck_browser_will_show_options_menu: useless
-"""
 
 import os
+import random
+
 from anki.utils import pointVersion
-from anki.hooks import wrap
-import aqt
 from aqt import mw
 from aqt import gui_hooks
-from aqt.deckbrowser import DeckBrowser
+
+from .config import addon_path, addonfoldername, gc
+from .adjust_css_files import adjust_deckbrowser_css, adjust_toolbar_css
 
 
-def gc(arg, fail=False):
-    conf = mw.addonManager.getConfig(__name__)
-    if conf:
-        return conf.get(arg, fail)
-    return fail
-
-
-addon_path = os.path.dirname(__file__)
-addonfoldername = os.path.basename(addon_path)
-regex = r"(user_files.*|web.*)"
-mw.addonManager.setWebExports(__name__, regex)
-
-
-fileversions_for_anki_version = {
-    "20": "css_files/20_deckbrowser.css",
-    "21": "css_files/20_deckbrowser.css",  # version for 20 and 21 are identical
+css_folder_for_anki_version = {
+    "22": "22",
+    "23": "22",  # example: for Anki version 23 use the contents of the folder 22 
 }
 
 
 v = pointVersion()
-if v in fileversions_for_anki_version:
-    dbcss = fileversions_for_anki_version[v]
+if v in css_folder_for_anki_version:
+    version_folder = css_folder_for_anki_version[v]
 else:  # for newer Anki versions try the latest version and hope for the best
-    dbcss = fileversions_for_anki_version[max(fileversions_for_anki_version, key=int)]
-
-dbcss_abs = os.path.join(addon_path, dbcss)
-imgname = gc("image name for file in user_files subfolder", "anki.png")
-img_web_rel_path  = f"/_addons/{addonfoldername}/user_files/{imgname}"
-merged = "web/deckbrowser.css"
-merged_abs = os.path.join(addon_path, merged)
-merged_web_rel_path  = f"/_addons/{addonfoldername}/{merged}"
-
-with open(dbcss_abs) as f:
-    cont = f.read()
-old = "body {"
-new = f"""{old}\nbackground-image: url("{img_web_rel_path}");"""
-cont = cont.replace(old, new)
+    version_folder = css_folder_for_anki_version[max(css_folder_for_anki_version, key=int)]
 
 
-with open(merged_abs, "w") as f:
-    f.write(cont)
+source_absolute = os.path.join(addon_path, "sources", "css", version_folder)
+web_absolute = os.path.join(addon_path, "web", "css")
+
+regex = r"(user_files.*|web.*)"
+mw.addonManager.setWebExports(__name__, regex)
 
 
+# on startup: combine template files with config and write into webexports folder
+change_copy = [os.path.basename(f) for f in os.listdir(source_absolute) if f.endswith(".css")]
+for f in change_copy:
+    with open(os.path.join(source_absolute, f)) as FO:
+        filecontent = FO.read()
+    if f == "deckbrowser.css":
+        filecontent = adjust_deckbrowser_css(filecontent)
+    if f == "toolbar.css" and gc("Toolbar image"):
+        filecontent = adjust_toolbar_css(filecontent)
+    with open(os.path.join(web_absolute, f), "w") as FO:
+        FO.write(filecontent)
+
+
+css_files_to_replace = [os.path.basename(f) for f in os.listdir(web_absolute) if f.endswith(".css")]
 def replace_css(web_content, context):
-    if isinstance(context, aqt.deckbrowser.DeckBrowser):
-        for idx, filename in enumerate(web_content.css):
-            if filename == "deckbrowser.css":
-                web_content.css[idx] = merged_web_rel_path
+    for idx, filename in enumerate(web_content.css):
+        if filename in css_files_to_replace:
+            web_content.css[idx] = f"/_addons/{addonfoldername}/web/css/{filename}"
 gui_hooks.webview_will_set_content.append(replace_css)
 
 
+def get_gearfile():
+    gear_abs = os.path.join(addon_path, "user_files", "gear")
+    gear_list = [os.path.basename(f) for f in os.listdir(gear_abs) if f.endswith((".svg", ".png"))]
+    val = gc("Image name for gear")
+    if val and val.lower() == "random":
+        return random.choice(gear_list)
+    if val in gear_list:
+        return val
+    else:
+        # if empty or illegal value try to use 'AnKing.png' to signal that an illegal values was
+        # used AnKing's gears folder doesn't contain a file named "gears.svg"
+        if "AnKing.png" in gear_list:
+            return "AnKing.png"
+        else:
+            return ""
+
 
 def replace_gears(deck_browser, content):
-    print(f"in replace_gears and type of content.tree is {type(content.tree)}")  # that's a string
     old = """<img src='/_anki/imgs/gears.svg'"""
-    new = f"""<img src='/_addons/{addonfoldername}/web/wikigear.svg'"""
+    new = f"""<img src='/_addons/{addonfoldername}/user_files/gear/{get_gearfile()}'"""
     content.tree = content.tree.replace(old, new)
 gui_hooks.deck_browser_will_render_content.append(replace_gears)
-
-
-## discarded unfinised idea: use js to modify the css after loading
-'''
-# This doesn't work:
-myjs = """
-console.log("%s");
-var bg = document.getElementById("body");
-bg.style.backgroundImage = url("%s");
-""" % (img_web_rel_path, img_web_rel_path)
-
-def showimage(deckbrowserinstance):
-    deckbrowserinstance.web.eval(myjs)
-# DeckBrowser.refresh = wrap(DeckBrowser.refresh, showimage)
-gui_hooks.deck_browser_did_render.append(showimage)
-'''
--- src/config.md
+++ src/config.md
@@ -1,1 +1,23 @@
-`image name for file in user_files subfolder`: name of the background file. You can add custom images to the folder "user_files" in the add-on folder. You see this folder if you click the button "View Files" on the right of the add-on manager window when you have selected this add-on. You must restart Anki so that changes take effect.
+<div style="color: red; font-size: 16px; background-color: rgb(25, 25, 25); text-align: center;"><br>Restart is required for any change to take effect<br></div>
+<hr>
+
+Created by ijgnd & [The AnKing](https://www.ankingmed.com). 
+
+If you enjoy this add-on or want individualized Anki help, please consider supporting [The AnKing](https://www.ankingmed.com) on **[Patreon](https://www.patreon.com/ankingmed)**, **[YouTube](https://www.youtube.com/theanking)** and **[Instagram](https://www.instagram.com/ankingmed)**/**[Facebook](https://www.facebook.com/ankingmed)** Thanks!
+
+---
+
+`You can add custom images to the folders "background" and "gears" These are located within the "user_files" folder in the add-on folder. You see this folder if you click the button "View Files" on the right of the add-on manager window when you have selected this add-on. I would recommend using 20-50% opacity images for the background.`
+
+* **Image name for background:** _(For no image use "". "Random" will shuffle through defaults)_ name of the background image file. 
+    * preloaded images (all .png include: AnKing, Beach, BeachAerial, Christ&Surgeon, ColorfulLights, Fire, Island, Milkyway, MoonMountains, NightSky, Ocean, SLCtemple, Sunset, SunsetMountain
+* **Image name for gear:** _(Anki default is "gears.svg". "Random" will shuffle through defaults)_ name of the file to replace the gear icon. (preloaded images include AnKing.png, flame.svg)
+    * preloaded images (all .png) include: AnKing, Cowboy, Crown, Cyclone, Fire, Firecracker, Globe, HeartEyes, Nerd, Poop, Skull, Soccer, Star, Sunglasses
+* **Toolbar image:** _(true or false)_ show the background image in the top and bottom toolbars in addition to the main screen
+
+`The following are css values and can be styled with appropriate css values for these css properties. See w3schools.com for more information.`
+
+* **background-attachment:** _(default "fixed")_ scroll or fixed
+* **background-position:** _(default "center")_ left top, right bottom, 25% 50%, 100px 200px, etc
+* **background-repeat:** _(default "no-repeat")_ repeat, repeat-x, repeat-y, no-repeat, etc
+* **background-size:** _(default "contain", but I would recommend cover if you set "Toolbar image" to true)_ contain, cover, 50%, 100px, etc 
